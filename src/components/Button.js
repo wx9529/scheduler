6b56a881cbc5120e4aa9f00f02cfe007cca0ed76
wx9529 <xinwang213z@gmail.com>
2022-04-19 13:46:02 -0700

    pass button tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  const btnClass = classNames(
+    "button",
+    { "button--confirm": props.confirm },
+    { "button--danger": props.danger }
+  );
+
+  return (
+    <button
+      className={btnClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
